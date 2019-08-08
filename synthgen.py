284525095c033adecdad75bfe80e2dd4e2dc3d68
IcyW <hbj032215@yeah.net>
2019-08-08 21:00:53 +0800

    *)modify RETR_CCOMP & CHAIN_APPROX_SIMPLE

--- synthgen.py
+++ synthgen.py
@@ -218,8 +218,8 @@ def get_text_placement_mask(xyz, mask, plane, pad=2, viz=False):
     PAD : number of pixels to pad the placement-mask by
     """
     contour, hier = cv2.findContours(mask.copy().astype('uint8'),
-                                     mode=cv2.cv.CV_RETR_CCOMP,
-                                     method=cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+                                     mode=cv2.RETR_CCOMP,
+                                     method=cv2.CHAIN_APPROX_SIMPLE)
     contour = [np.squeeze(c).astype('float') for c in contour]
     # plane = np.array([plane[1],plane[0],plane[2],plane[3]])
     H, W = mask.shape[:2]
